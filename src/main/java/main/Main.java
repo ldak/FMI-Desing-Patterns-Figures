118f2c2bd7116ae4fa2b8f45ed532adca1adfc84
Milen <milen.com1@gmail.com>
2024-11-12 15:29:54 +0200

    Main implemented

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,16 @@
 package main;
 
+import commands.CommandFactory;
+import figures.factories.FigureFactoryFactory;
+import utils.FigureReflectionFacade;
+import utils.SystemExitHandler;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CommandFactory commandFactory = new CommandFactory(new SystemExitHandler());
+        FigureFactoryFactory figureFactoryFactory = new FigureFactoryFactory(new FigureReflectionFacade());
+
+        CommandLineProgram program = new CommandLineProgram(commandFactory, figureFactoryFactory);
+        program.start();
     }
 }
